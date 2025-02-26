1e9333a0abe2960e6d291cb0ff9d1a3897b8156a
Hellboy-Nishero <Nik.Nisher@gmail.com>
2025-02-26 17:39:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+  //Перед работой устанавливаем:
+  //sass-embeded, react-router, react-hook-form, redux/redux-toolkit, lucide-react
+
+  return (
+    <>
+    {/* navbar */}
+      Hello world
+      {/* footer */}
+    </>
+  )
+}
+
+export default App
