c83831a44aa1dc2235ba145dcac2b892e0e90293
Ihor <batbko97@gmail.com>
2025-03-07 11:51:15 +0100

    Первичная итерация страницы Home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./components/HomeBanner.jsx";
+import HomeDiscount from "./components/HomeDiscount.jsx";
+
+const MainPage = () => {
+  return (
+    <>
+      <Banner />
+      <HomeDiscount />
+    </>
+  );
+};
+
+export default MainPage;
