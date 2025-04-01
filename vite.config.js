a5455548bed472a8c797617972d289262ae15893
Hellboy-Nishero <142092453+Hellboy-Nishero@users.noreply.github.com>
2025-04-01 11:47:23 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "./",
-  server:{
-    headers: {
-      "Content-Type": "application/javascript"
-    }
-  }
+  plugins: [react()]
 })
